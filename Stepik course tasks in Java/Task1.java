ea370d7c35ae2b84b28307009271633472f07f13
katesem <50136680+katesem@users.noreply.github.com>
2021-03-26 14:44:02 +0200

    Task 1 implemented in Java + Selenium

---
+++ Stepik course tasks in Java/Task1.java
@@ -0,0 +1,36 @@
+ackage com.company;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\kate_s\\Downloads\\chromedriver\\chromedriver.exe"); //define path to the webdriver
+        WebDriver driver = new ChromeDriver(); //   Create instance of ChromeDriver interface
+    try
+        {
+        driver.get("http://suninjuly.github.io/huge_form.html");
+        List<WebElement> elements = driver.findElements(By.tagName("input"));
+
+        for (WebElement element : elements) {
+            element.sendKeys("Answer");
+        }
+
+        WebElement button = driver.findElement(By.cssSelector("button.btn"));
+        button.click();
+       }
+
+finally
+        {
+            TimeUnit.SECONDS.sleep(6);
+            driver.quit();
+        }
+    }
+}
